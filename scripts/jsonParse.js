8393760c1fb864dd88db4e5b1919943acefec301
jcochran206 <jonathan.o.cochran@gmail.com>
2023-08-14 22:38:05 -0700

    added function to iterate thru the json data

--- scripts/jsonParse.js
+++ scripts/jsonParse.js
@@ -1,10 +1,18 @@
 // add data from file 
 const data = require('../data/pac12.json');
 
-// test data is visiable to file
+let jsonObjects = data.features;
 
+// test data is visiable to file
+// console.log(jsonObjects, 'is data visible');
 
 // iterate thru data (read properties name, tenant, city, state)
+function jsonReader(json){
+    for(index in json){
+        let obj = json[index];
+        console.log(obj.properties.Tenant)
+    }
+}
 
-
-// 
+//call back
+console.log(jsonReader(jsonObjects));
